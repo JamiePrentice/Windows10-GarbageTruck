fa402612b9f13e394b4705a0ccc080f9e8377484
Jamie Prentice <2858046+JamiePrentice@users.noreply.github.com>
2017-11-29 08:43:35 +0000

    Doing way more with way less. Single line Appx removal.

--- GarbageTruck.ps1
+++ GarbageTruck.ps1
@@ -1,36 +1,5 @@
-get-appxpackage -allusers *3d* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *3dbuilder* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *alarms* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *appconnector* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *appinstaller* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *bingfinance* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *bingnews* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *bingsports* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *bingweather* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *calculator* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *camera* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *communicationsapps* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *connectivitystore* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *feedback* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *gethelp* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *getstarted* | remove-appxpackage | Out-Null
-# get-appxpackage -allusers  *holographic* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *maps* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *messaging* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *mspaint* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *officehub* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *oneconnect* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *onenote* | remove-appxpackage | Out-Null
-# get-appxpackage -allusers  *people* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *phone* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *phone* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *skypeapp* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *solitaire* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *soundrecorder* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *sway* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *wallet* | remove-appxpackage | Out-Null
-# get-appxpackage -allusers  *xbox* | remove-appxpackage | Out-Null
-get-appxpackage -allusers  *zune* | remove-appxpackage | Out-Null
+# Remove all Appx Packages except Store, Calculator and Photos
+Get-AppxPackage -allusers | where-object {$_.name –notlike "*store*"} | where-object {$_.name –notlike "*photos*"} | where-object {$_.name –notlike "*calculator*"} | Remove-AppxPackage -erroraction 'silentlycontinue'
 
 # Disable Cortana
 New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Windows Search" -Name "AllowCortana" -Value "0" ` -PropertyType DWORD -Force | Out-Null
@@ -39,11 +8,13 @@ New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Windows Search
 New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Windows Search" -Name "ConnectedSearchUseWeb" -Value "0" ` -PropertyType DWORD -Force | Out-Null
 New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Windows Search" -Name "ConnectedSearchUseWebOverMeteredConnections" -Value "0" ` -PropertyType DWORD -Force | Out-Null
 New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Windows Search" -Name "DisableWebSearch" -Value "1" ` -PropertyType DWORD -Force | Out-Null
+
 # Disable OneDrive
 New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSync" -Value "1" ` -PropertyType DWORD -Force | Out-Null
 New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSyncNGSC" -Value "1" ` -PropertyType DWORD -Force | Out-Null
 New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\OneDrive" -Name "DisableLibrariesDefaultSaveToOneDrive" -Value "1" ` -PropertyType DWORD -Force | Out-Null
 New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\OneDrive" -Name "DisableLocation" -Value "1" ` -PropertyType DWORD -Force | Out-Null
+
 # Disable GameDVR and GameUX
 New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\GameDVR" -Name "AllowGameDVR" -Value "0" ` -PropertyType DWORD -Force | Out-Null
 New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\GameUX" -Name "DownloadGameInfo" -Value "0" ` -PropertyType DWORD -Force | Out-Null
